7447faa17de11b084e8978256da823da04de3458
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-04-23 11:16:03 +0100

    users side of things a bit more polished registration updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  # devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root "statics#home"
   resources :users, only: [:index, :show]
+
+
+
 
 end
